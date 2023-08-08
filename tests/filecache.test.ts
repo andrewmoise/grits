97b7d2b7df6c104d7a0f719689e1abc64de52557
Andrew Moise <andrew.moise@gmail.com>
2023-08-08 10:18:03 -0400

    Update filecache test

--- tests/filecache.test.ts
+++ tests/filecache.test.ts
@@ -8,7 +8,7 @@ import * as crypto from 'crypto';
 test('add a file and read from the cache', async () => {
     // Arrange
     const cacheDir = await fs.mkdtemp(path.join(os.tmpdir(), 'cache-'));
-    const config = new Config();
+    const config = new Config('127.0.0.1', 1787);
     config.storageDirectory = cacheDir;
     config.storageSize = 5000000; // 5MB cache size
     const fileCache = new FileCache(config);
@@ -17,18 +17,16 @@ test('add a file and read from the cache', async () => {
     const testFilePath = path.join(cacheDir, 'testFile');
     await fs.writeFile(testFilePath, testFileContent);
 
-    const hash = crypto.createHash('sha1');
-    hash.update(testFileContent);
-    const fileHash = hash.digest('hex');
-
     // Act
-    await fileCache.addFile(testFilePath, fileHash);
+    const writtenFile = await fileCache.addFile(testFilePath);
+    const cachedFile = await fileCache.readFile(writtenFile.fileAddr);
+    
+    expect(cachedFile).not.toBeNull();
 
-    const buffer = await fileCache.readFile(fileHash, 0,
-        testFileContent.length);
-
+    const buffer = await cachedFile!.read(0, testFileContent.length);
+    
     // Assert
-    expect(buffer !== null);
+    expect(buffer).not.toBeNull();
     expect(buffer!.toString()).toEqual(testFileContent);
 
     // Clean up
@@ -40,7 +38,7 @@ test('add files to overflow the cache and verify the cache behaves correctly',
         // Arrange
         const cacheDir = await fs.mkdtemp(path.join(os.tmpdir(), 'cache-'));
 
-        const config = new Config();
+        const config = new Config('127.0.0.1', 1787);
         config.storageDirectory = cacheDir;
         config.storageSize = 1000000; // 1MB cache size
 
@@ -57,27 +55,25 @@ test('add files to overflow the cache and verify the cache behaves correctly',
             const testFilePath = path.join(cacheDir, 'testFile' + i);
             await fs.writeFile(testFilePath, testFileContent);
 
-            const hash = crypto.createHash('sha1');
-            hash.update(testFileContent);
-            const fileHash = hash.digest('hex');
-
-            await fileCache.addFile(testFilePath, fileHash);
-            fileHashes.push(fileHash);
+            const writtenFile = await fileCache.addFile(testFilePath);
+            fileHashes.push(writtenFile.fileAddr);
         }
 
         // Assert
         // Check that the first 10 files are not in the cache
         for (let i = 0; i < 10; i++) {
-            const buffer = await fileCache.readFile(
-                fileHashes[i], 0, fileSize);
-            expect(buffer === null);
+            const cachedFile = await fileCache.readFile(fileHashes[i]);
+            expect(cachedFile).toBeNull();
         }
 
         // Check that the last 10 files are in the cache
         for (let i = 10; i < numberOfFiles; i++) {
-            const buffer = await fileCache.readFile(
-                fileHashes[i], 0, fileSize);
-            expect(buffer !== null);
+            const cachedFile = await fileCache.readFile(fileHashes[i]);
+            expect(cachedFile).not.toBeNull();
+
+            const buffer = await cachedFile!.read(0, fileSize);
+            expect(buffer).not.toBeNull();
+            
             const testFileContent = String(i).padStart(2, '0').repeat(
                 fileSize / 2);
             expect(buffer!.toString()).toEqual(testFileContent);
