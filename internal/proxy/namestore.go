e54df35da81b7254357ca12f879e4270bc6a41e9
Andrew Moise <andrew.moise@gmail.com>
2024-02-06 14:28:38 -0500

    General WIP within the rough cut

--- internal/proxy/namestore.go
+++ internal/proxy/namestore.go
@@ -6,33 +6,38 @@ import (
 )
 
 type NameStore struct {
-	names map[string]*grits.FileAddr
-	mtx   sync.RWMutex
+	root *FileNode
+	mtx  sync.RWMutex
 }
 
-func NewNameStore() *NameStore {
-	return &NameStore{
-		names: make(map[string]*grits.FileAddr),
+func NewNameStore(bs *BlobStore) (*NameStore, error) {
+	fn, err := bs.CreateFileNode(make(map[string]*grits.FileAddr))
+	if err != nil {
+		return nil, err
 	}
+
+	return &NameStore{
+		root: fn,
+	}, nil
 }
 
 // MapNameToBlob associates a human-readable name with a blob address.
 func (ns *NameStore) MapNameToBlob(name string, addr *grits.FileAddr) {
 	ns.mtx.Lock()
 	defer ns.mtx.Unlock()
-	ns.names[name] = addr
+	ns.root.Children[name] = addr
 }
 
 // ResolveName retrieves the blob address associated with a given name.
 func (ns *NameStore) ResolveName(name string) (*grits.FileAddr, bool) {
 	ns.mtx.RLock()
 	defer ns.mtx.RUnlock()
-	addr, exists := ns.names[name]
+	addr, exists := ns.root.Children[name]
 	return addr, exists
 }
 
 func (ns *NameStore) RemoveName(name string) {
 	ns.mtx.Lock()
 	defer ns.mtx.Unlock()
-	delete(ns.names, name)
+	delete(ns.root.Children, name)
 }
--- internal/proxy/nodes.go
+++ internal/proxy/nodes.go
@@ -1,33 +1,17 @@
 package proxy
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"grits/internal/grits"
 )
 
 type FileNode struct {
 	ExportedBlob *grits.CachedFile          `json:"-"` // This field is ignored by the JSON package.
 	Children     map[string]*grits.FileAddr // Maps file names to their CachedFile
-}
-
-// NewFileNode creates a new FileNode.
-func NewFileNode() *FileNode {
-	return &FileNode{
-		ExportedBlob: nil,
-		Children:     make(map[string]*grits.FileAddr),
-	}
-}
-
-func CloneFileNode(node *FileNode) *FileNode {
-	clone := &FileNode{
-		ExportedBlob: nil,
-		Children:     make(map[string]*grits.FileAddr),
-	}
-
-	for name, child := range node.Children {
-		clone.Children[name] = child
-	}
-
-	return clone
 }
 
 // AddFile adds a file to the FileNode.
@@ -52,11 +36,49 @@ func (fn *FileNode) RemoveFile(name string) {
 	delete(fn.Children, name)
 }
 
+// FetchFileNode retrieves a FileNode from the blob store using its hash.
+func (bs *BlobStore) FetchFileNode(hash string) (*FileNode, error) {
+	// If not cached, read from the store, deserialize, and cache
+	path := filepath.Join(bs.config.StorageDirectory, hash)
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("error reading FileNode from path %s: %v", path, err)
+	}
+
+	var node FileNode
+	if err := json.Unmarshal(data, &node); err != nil {
+		return nil, fmt.Errorf("error deserializing FileNode: %v", err)
+	}
+
+	//bs.fileNodes[hash] = &node // Cache it
+	return &node, nil
+}
+
+// CreateFileNode creates a FileNode with the specified children,
+// serializes it, stores it in the blob store, and caches it.
+func (bs *BlobStore) CreateFileNode(children map[string]*grits.FileAddr) (*FileNode, error) {
+	node := &FileNode{
+		Children: children,
+	}
+	data, err := json.Marshal(node)
+	if err != nil {
+		return nil, fmt.Errorf("error serializing FileNode: %v", err)
+	}
+
+	node.ExportedBlob, err = bs.AddDataBlock(data)
+	if err != nil {
+		return nil, fmt.Errorf("error storing FileNode: %v", err)
+	}
+
+	//bs.fileNodes[node.ExportedBlob.Address.String()] = node // Cache it
+	return node, nil
+}
+
 // RevNode represents a revision, containing a snapshot of the content at a point in time.
 type RevNode struct {
-	ExportedBlob *grits.CachedFile
-	Tree         *FileNode // The current state of the content
-	Previous     *RevNode  // Pointer to the previous revision, nil if it's the first
+	ExportedBlob *grits.CachedFile `json:"-"`
+	Tree         *FileNode         // The current state of the content
+	Previous     *RevNode          // Pointer to the previous revision, nil if it's the first
 }
 
 // NewRevNode creates a new instance of RevNode.
