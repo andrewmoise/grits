36d8984c94ed24680fd165d9acf69994614cf9db
Andrew Moise <andrew.moise@gmail.com>
2024-02-17 08:47:25 -0500

    Add periodic tasks, currently incl checkpointing the account namespaces

---
+++ internal/server/tasks.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"log"
+	"time"
+)
+
+func (s *Server) AddPeriodicTask(interval time.Duration, task func() error) {
+	ticker := time.NewTicker(interval)
+	s.taskWg.Add(1)
+	go func() {
+		defer ticker.Stop()
+		defer s.taskWg.Done()
+		for {
+			select {
+			case <-ticker.C:
+				err := task()
+				if err != nil {
+					log.Printf("Periodic task failed: %v\n", err)
+				}
+			case <-s.taskStop:
+				return
+			}
+		}
+	}()
+}
+
+func (s *Server) StopPeriodicTasks() {
+	close(s.taskStop)
+	s.taskWg.Wait()
+}
