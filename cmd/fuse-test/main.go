2d78db2c91546d0ff4a13bf6ba5ab6991fa020e8
Andrew Moise <moise@melanic.org>
2024-04-28 15:29:57 +0000

    Move a bunch of stuff into main from the fuse modules

--- cmd/fuse-test/main.go
+++ cmd/fuse-test/main.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io"
@@ -17,258 +18,13 @@ import (
 	"sync"
 	"syscall"
 	"time"
-	"unsafe"
 
 	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
 	"github.com/hanwen/go-fuse/v2/fuse/nodefs"
 	"github.com/hanwen/go-fuse/v2/fuse/pathfs"
+	"golang.org/x/sys/unix"
 )
-
-/////
-// File stuff
-
-// LoopbackFile delegates all operations back to an underlying os.File.
-func NewLoopbackFile(f *os.File) nodefs.File {
-	return &loopbackFile{File: f}
-}
-
-type loopbackFile struct {
-	File *os.File
-
-	// os.File is not threadsafe. Although fd themselves are
-	// constant during the lifetime of an open file, the OS may
-	// reuse the fd number after it is closed. When open races
-	// with another close, they may lead to confusion as which
-	// file gets written in the end.
-	lock sync.Mutex
-}
-
-func (f *loopbackFile) InnerFile() nodefs.File {
-	return nil
-}
-
-func (f *loopbackFile) SetInode(n *nodefs.Inode) {
-}
-
-func (f *loopbackFile) String() string {
-	return fmt.Sprintf("loopbackFile(%s)", f.File.Name())
-}
-
-func (f *loopbackFile) Read(buf []byte, off int64) (res fuse.ReadResult, code fuse.Status) {
-	f.lock.Lock()
-	// This is not racy by virtue of the kernel properly
-	// synchronizing the open/write/close.
-	r := fuse.ReadResultFd(f.File.Fd(), off, len(buf))
-	f.lock.Unlock()
-	return r, fuse.OK
-}
-
-func (f *loopbackFile) Write(data []byte, off int64) (uint32, fuse.Status) {
-	f.lock.Lock()
-	n, err := f.File.WriteAt(data, off)
-	f.lock.Unlock()
-	return uint32(n), fuse.ToStatus(err)
-}
-
-func (f *loopbackFile) Release() {
-	f.lock.Lock()
-	f.File.Close()
-	f.lock.Unlock()
-}
-
-func (f *loopbackFile) Flush() fuse.Status {
-	f.lock.Lock()
-
-	// Since Flush() may be called for each dup'd fd, we don't
-	// want to really close the file, we just want to flush. This
-	// is achieved by closing a dup'd fd.
-	newFd, err := syscall.Dup(int(f.File.Fd()))
-	f.lock.Unlock()
-
-	if err != nil {
-		return fuse.ToStatus(err)
-	}
-	err = syscall.Close(newFd)
-	return fuse.ToStatus(err)
-}
-
-func (f *loopbackFile) Fsync(flags int) (code fuse.Status) {
-	f.lock.Lock()
-	r := fuse.ToStatus(syscall.Fsync(int(f.File.Fd())))
-	f.lock.Unlock()
-
-	return r
-}
-
-const (
-	F_OFD_GETLK  = 36
-	F_OFD_SETLK  = 37
-	F_OFD_SETLKW = 38
-)
-
-func (f *loopbackFile) GetLk(owner uint64, lk *fuse.FileLock, flags uint32, out *fuse.FileLock) (code fuse.Status) {
-	flk := syscall.Flock_t{}
-	lk.ToFlockT(&flk)
-	code = fuse.ToStatus(syscall.FcntlFlock(f.File.Fd(), F_OFD_GETLK, &flk))
-	out.FromFlockT(&flk)
-	return
-}
-
-func (f *loopbackFile) SetLk(owner uint64, lk *fuse.FileLock, flags uint32) (code fuse.Status) {
-	return f.setLock(owner, lk, flags, false)
-}
-
-func (f *loopbackFile) SetLkw(owner uint64, lk *fuse.FileLock, flags uint32) (code fuse.Status) {
-	return f.setLock(owner, lk, flags, true)
-}
-
-func (f *loopbackFile) setLock(owner uint64, lk *fuse.FileLock, flags uint32, blocking bool) (code fuse.Status) {
-	if (flags & fuse.FUSE_LK_FLOCK) != 0 {
-		var op int
-		switch lk.Typ {
-		case syscall.F_RDLCK:
-			op = syscall.LOCK_SH
-		case syscall.F_WRLCK:
-			op = syscall.LOCK_EX
-		case syscall.F_UNLCK:
-			op = syscall.LOCK_UN
-		default:
-			return fuse.EINVAL
-		}
-		if !blocking {
-			op |= syscall.LOCK_NB
-		}
-		return fuse.ToStatus(syscall.Flock(int(f.File.Fd()), op))
-	} else {
-		flk := syscall.Flock_t{}
-		lk.ToFlockT(&flk)
-		var op int
-		if blocking {
-			op = F_OFD_SETLKW
-		} else {
-			op = F_OFD_SETLK
-		}
-		return fuse.ToStatus(syscall.FcntlFlock(f.File.Fd(), op, &flk))
-	}
-}
-
-func (f *loopbackFile) Truncate(size uint64) fuse.Status {
-	f.lock.Lock()
-	r := fuse.ToStatus(syscall.Ftruncate(int(f.File.Fd()), int64(size)))
-	f.lock.Unlock()
-
-	return r
-}
-
-func (f *loopbackFile) Chmod(mode uint32) fuse.Status {
-	f.lock.Lock()
-	r := fuse.ToStatus(f.File.Chmod(os.FileMode(mode)))
-	f.lock.Unlock()
-
-	return r
-}
-
-func (f *loopbackFile) Chown(uid uint32, gid uint32) fuse.Status {
-	f.lock.Lock()
-	r := fuse.ToStatus(f.File.Chown(int(uid), int(gid)))
-	f.lock.Unlock()
-
-	return r
-}
-
-func (f *loopbackFile) GetAttr(a *fuse.Attr) fuse.Status {
-	st := syscall.Stat_t{}
-	f.lock.Lock()
-	err := syscall.Fstat(int(f.File.Fd()), &st)
-	f.lock.Unlock()
-	if err != nil {
-		return fuse.ToStatus(err)
-	}
-	a.FromStat(&st)
-
-	return fuse.OK
-}
-
-// Utimens implemented in files_linux.go
-
-// Allocate implemented in files_linux.go
-
-////////////////////////////////////////////////////////////////
-
-// NewReadOnlyFile wraps a File so all write operations are denied.
-func NewReadOnlyFile(f nodefs.File) nodefs.File {
-	return &readOnlyFile{File: f}
-}
-
-type readOnlyFile struct {
-	nodefs.File
-}
-
-func (f *readOnlyFile) InnerFile() nodefs.File {
-	return f.File
-}
-
-func (f *readOnlyFile) String() string {
-	return fmt.Sprintf("readOnlyFile(%s)", f.File.String())
-}
-
-func (f *readOnlyFile) Write(data []byte, off int64) (uint32, fuse.Status) {
-	return 0, fuse.EPERM
-}
-
-func (f *readOnlyFile) Fsync(flag int) (code fuse.Status) {
-	return fuse.OK
-}
-
-func (f *readOnlyFile) Truncate(size uint64) fuse.Status {
-	return fuse.EPERM
-}
-
-func (f *readOnlyFile) Chmod(mode uint32) fuse.Status {
-	return fuse.EPERM
-}
-
-func (f *readOnlyFile) Chown(uid uint32, gid uint32) fuse.Status {
-	return fuse.EPERM
-}
-
-func (f *readOnlyFile) Allocate(off uint64, sz uint64, mode uint32) fuse.Status {
-	return fuse.EPERM
-}
-
-// Note: This stuff is Linux only:
-
-func (f *loopbackFile) Allocate(off uint64, sz uint64, mode uint32) fuse.Status {
-	f.lock.Lock()
-	err := syscall.Fallocate(int(f.File.Fd()), mode, int64(off), int64(sz))
-	f.lock.Unlock()
-	if err != nil {
-		return fuse.ToStatus(err)
-	}
-	return fuse.OK
-}
-
-// Utimens - file handle based version of loopbackFileSystem.Utimens()
-func (f *loopbackFile) Utimens(a *time.Time, m *time.Time) fuse.Status {
-	var ts [2]syscall.Timespec
-	ts[0] = fuse.UtimeToTimespec(a)
-	ts[1] = fuse.UtimeToTimespec(m)
-	f.lock.Lock()
-	err := futimens(int(f.File.Fd()), &ts)
-	f.lock.Unlock()
-	return fuse.ToStatus(err)
-}
-
-// futimens - futimens(3) calls utimensat(2) with "pathname" set to null and
-// "flags" set to zero
-func futimens(fd int, times *[2]syscall.Timespec) (err error) {
-	_, _, e1 := syscall.Syscall6(syscall.SYS_UTIMENSAT, uintptr(fd), 0, uintptr(unsafe.Pointer(times)), uintptr(0), 0, 0)
-	if e1 != 0 {
-		err = syscall.Errno(e1)
-	}
-	return
-}
 
 /////
 // Filesystem stuff
@@ -506,6 +262,467 @@ func (fs *loopbackFileSystem) Utimens(path string, a *time.Time, m *time.Time, c
 	//return fuse.ToStatus(err)
 
 	return fuse.EACCES
+}
+
+/////
+// Loopback stuff
+
+// LoopbackRoot holds the parameters for creating a new loopback
+// filesystem. Loopback filesystem delegate their operations to an
+// underlying POSIX file system.
+type LoopbackRoot struct {
+	// The path to the root of the underlying file system.
+	Path string
+
+	// The device on which the Path resides. This must be set if
+	// the underlying filesystem crosses file systems.
+	Dev uint64
+
+	// NewNode returns a new InodeEmbedder to be used to respond
+	// to a LOOKUP/CREATE/MKDIR/MKNOD opcode. If not set, use a
+	// LoopbackNode.
+	NewNode func(rootData *LoopbackRoot, parent *fs.Inode, name string, st *syscall.Stat_t) fs.InodeEmbedder
+}
+
+func (r *LoopbackRoot) newNode(parent *fs.Inode, name string, st *syscall.Stat_t) fs.InodeEmbedder {
+	if r.NewNode != nil {
+		return r.NewNode(r, parent, name, st)
+	}
+	return &LoopbackNode{
+		RootData: r,
+	}
+}
+
+func (r *LoopbackRoot) idFromStat(st *syscall.Stat_t) fs.StableAttr {
+	// We compose an inode number by the underlying inode, and
+	// mixing in the device number. In traditional filesystems,
+	// the inode numbers are small. The device numbers are also
+	// small (typically 16 bit). Finally, we mask out the root
+	// device number of the root, so a loopback FS that does not
+	// encompass multiple mounts will reflect the inode numbers of
+	// the underlying filesystem
+	swapped := (uint64(st.Dev) << 32) | (uint64(st.Dev) >> 32)
+	swappedRootDev := (r.Dev << 32) | (r.Dev >> 32)
+	return fs.StableAttr{
+		Mode: uint32(st.Mode),
+		Gen:  1,
+		// This should work well for traditional backing FSes,
+		// not so much for other go-fuse FS-es
+		Ino: (swapped ^ swappedRootDev) ^ st.Ino,
+	}
+}
+
+// NewLoopbackRoot returns a root node for a loopback file system whose
+// root is at the given root. This node implements all NodeXxxxer
+// operations available.
+func NewLoopbackRoot(rootPath string) (fs.InodeEmbedder, error) {
+	var st syscall.Stat_t
+	err := syscall.Stat(rootPath, &st)
+	if err != nil {
+		return nil, err
+	}
+
+	root := &LoopbackRoot{
+		Path: rootPath,
+		Dev:  uint64(st.Dev),
+	}
+
+	return root.newNode(nil, "", &st), nil
+}
+
+// LoopbackNode is a filesystem node in a loopback file system. It is
+// public so it can be used as a basis for other loopback based
+// filesystems. See NewLoopbackFile or LoopbackRoot for more
+// information.
+type LoopbackNode struct {
+	fs.Inode
+
+	// RootData points back to the root of the loopback filesystem.
+	RootData *LoopbackRoot
+}
+
+var _ = (fs.NodeStatfser)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Statfs(ctx context.Context, out *fuse.StatfsOut) syscall.Errno {
+	s := syscall.Statfs_t{}
+	err := syscall.Statfs(n.path(), &s)
+	if err != nil {
+		return fs.ToErrno(err)
+	}
+	out.FromStatfsT(&s)
+	return fs.OK
+}
+
+// path returns the full path to the file in the underlying file
+// system.
+func (n *LoopbackNode) path() string {
+	path := n.Path(n.Root())
+	return filepath.Join(n.RootData.Path, path)
+}
+
+var _ = (fs.NodeLookuper)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+	p := filepath.Join(n.path(), name)
+
+	st := syscall.Stat_t{}
+	err := syscall.Lstat(p, &st)
+	if err != nil {
+		return nil, fs.ToErrno(err)
+	}
+
+	out.Attr.FromStat(&st)
+	node := n.RootData.newNode(n.EmbeddedInode(), name, &st)
+	ch := n.NewInode(ctx, node, n.RootData.idFromStat(&st))
+	return ch, 0
+}
+
+// preserveOwner sets uid and gid of `path` according to the caller information
+// in `ctx`.
+func (n *LoopbackNode) preserveOwner(ctx context.Context, path string) error {
+	if os.Getuid() != 0 {
+		return nil
+	}
+	caller, ok := fuse.FromContext(ctx)
+	if !ok {
+		return nil
+	}
+	return syscall.Lchown(path, int(caller.Uid), int(caller.Gid))
+}
+
+var _ = (fs.NodeMknoder)((*LoopbackNode)(nil))
+
+func intDev(dev uint32) int {
+	return int(dev)
+}
+
+func (n *LoopbackNode) Mknod(ctx context.Context, name string, mode, rdev uint32, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+	p := filepath.Join(n.path(), name)
+	err := syscall.Mknod(p, mode, intDev(rdev))
+	if err != nil {
+		return nil, fs.ToErrno(err)
+	}
+	n.preserveOwner(ctx, p)
+	st := syscall.Stat_t{}
+	if err := syscall.Lstat(p, &st); err != nil {
+		syscall.Rmdir(p)
+		return nil, fs.ToErrno(err)
+	}
+
+	out.Attr.FromStat(&st)
+
+	node := n.RootData.newNode(n.EmbeddedInode(), name, &st)
+	ch := n.NewInode(ctx, node, n.RootData.idFromStat(&st))
+
+	return ch, 0
+}
+
+var _ = (fs.NodeMkdirer)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Mkdir(ctx context.Context, name string, mode uint32, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+	p := filepath.Join(n.path(), name)
+	err := os.Mkdir(p, os.FileMode(mode))
+	if err != nil {
+		return nil, fs.ToErrno(err)
+	}
+	n.preserveOwner(ctx, p)
+	st := syscall.Stat_t{}
+	if err := syscall.Lstat(p, &st); err != nil {
+		syscall.Rmdir(p)
+		return nil, fs.ToErrno(err)
+	}
+
+	out.Attr.FromStat(&st)
+
+	node := n.RootData.newNode(n.EmbeddedInode(), name, &st)
+	ch := n.NewInode(ctx, node, n.RootData.idFromStat(&st))
+
+	return ch, 0
+}
+
+var _ = (fs.NodeRmdirer)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Rmdir(ctx context.Context, name string) syscall.Errno {
+	p := filepath.Join(n.path(), name)
+	err := syscall.Rmdir(p)
+	return fs.ToErrno(err)
+}
+
+var _ = (fs.NodeUnlinker)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Unlink(ctx context.Context, name string) syscall.Errno {
+	p := filepath.Join(n.path(), name)
+	err := syscall.Unlink(p)
+	return fs.ToErrno(err)
+}
+
+var _ = (fs.NodeRenamer)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Rename(ctx context.Context, name string, newParent fs.InodeEmbedder, newName string, flags uint32) syscall.Errno {
+	if flags&fs.RENAME_EXCHANGE != 0 {
+		return n.renameExchange(name, newParent, newName)
+	}
+
+	p1 := filepath.Join(n.path(), name)
+	p2 := filepath.Join(n.RootData.Path, newParent.EmbeddedInode().Path(nil), newName)
+
+	err := syscall.Rename(p1, p2)
+	return fs.ToErrno(err)
+}
+
+var _ = (fs.NodeCreater)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Create(ctx context.Context, name string, flags uint32, mode uint32, out *fuse.EntryOut) (inode *fs.Inode, fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+	p := filepath.Join(n.path(), name)
+	flags = flags &^ syscall.O_APPEND
+	fd, err := syscall.Open(p, int(flags)|os.O_CREATE, mode)
+	if err != nil {
+		return nil, nil, 0, fs.ToErrno(err)
+	}
+	n.preserveOwner(ctx, p)
+	st := syscall.Stat_t{}
+	if err := syscall.Fstat(fd, &st); err != nil {
+		syscall.Close(fd)
+		return nil, nil, 0, fs.ToErrno(err)
+	}
+
+	node := n.RootData.newNode(n.EmbeddedInode(), name, &st)
+	ch := n.NewInode(ctx, node, n.RootData.idFromStat(&st))
+	lf := fs.NewLoopbackFile(fd)
+
+	out.FromStat(&st)
+	return ch, lf, 0, 0
+}
+
+func (n *LoopbackNode) renameExchange(name string, newparent fs.InodeEmbedder, newName string) syscall.Errno {
+	fd1, err := syscall.Open(n.path(), syscall.O_DIRECTORY, 0)
+	if err != nil {
+		return fs.ToErrno(err)
+	}
+	defer syscall.Close(fd1)
+	p2 := filepath.Join(n.RootData.Path, newparent.EmbeddedInode().Path(nil))
+	fd2, err := syscall.Open(p2, syscall.O_DIRECTORY, 0)
+	defer syscall.Close(fd2)
+	if err != nil {
+		return fs.ToErrno(err)
+	}
+
+	var st syscall.Stat_t
+	if err := syscall.Fstat(fd1, &st); err != nil {
+		return fs.ToErrno(err)
+	}
+
+	// Double check that nodes didn't change from under us.
+	inode := &n.Inode
+	if inode.Root() != inode && inode.StableAttr().Ino != n.RootData.idFromStat(&st).Ino {
+		return syscall.EBUSY
+	}
+	if err := syscall.Fstat(fd2, &st); err != nil {
+		return fs.ToErrno(err)
+	}
+
+	newinode := newparent.EmbeddedInode()
+	if newinode.Root() != newinode && newinode.StableAttr().Ino != n.RootData.idFromStat(&st).Ino {
+		return syscall.EBUSY
+	}
+
+	return fs.ToErrno(unix.Renameat2(fd1, name, fd2, newName, unix.RENAME_EXCHANGE))
+}
+
+var _ = (fs.NodeSymlinker)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Symlink(ctx context.Context, target, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+	p := filepath.Join(n.path(), name)
+	err := syscall.Symlink(target, p)
+	if err != nil {
+		return nil, fs.ToErrno(err)
+	}
+	n.preserveOwner(ctx, p)
+	st := syscall.Stat_t{}
+	if err := syscall.Lstat(p, &st); err != nil {
+		syscall.Unlink(p)
+		return nil, fs.ToErrno(err)
+	}
+	node := n.RootData.newNode(n.EmbeddedInode(), name, &st)
+	ch := n.NewInode(ctx, node, n.RootData.idFromStat(&st))
+
+	out.Attr.FromStat(&st)
+	return ch, 0
+}
+
+var _ = (fs.NodeLinker)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Link(ctx context.Context, target fs.InodeEmbedder, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+
+	p := filepath.Join(n.path(), name)
+	err := syscall.Link(filepath.Join(n.RootData.Path, target.EmbeddedInode().Path(nil)), p)
+	if err != nil {
+		return nil, fs.ToErrno(err)
+	}
+	st := syscall.Stat_t{}
+	if err := syscall.Lstat(p, &st); err != nil {
+		syscall.Unlink(p)
+		return nil, fs.ToErrno(err)
+	}
+	node := n.RootData.newNode(n.EmbeddedInode(), name, &st)
+	ch := n.NewInode(ctx, node, n.RootData.idFromStat(&st))
+
+	out.Attr.FromStat(&st)
+	return ch, 0
+}
+
+var _ = (fs.NodeReadlinker)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Readlink(ctx context.Context) ([]byte, syscall.Errno) {
+	p := n.path()
+
+	for l := 256; ; l *= 2 {
+		buf := make([]byte, l)
+		sz, err := syscall.Readlink(p, buf)
+		if err != nil {
+			return nil, fs.ToErrno(err)
+		}
+
+		if sz < len(buf) {
+			return buf[:sz], 0
+		}
+	}
+}
+
+var _ = (fs.NodeOpener)((*LoopbackNode)(nil))
+
+type loopbackFile struct {
+	mu sync.Mutex
+	fd int
+}
+
+func NewLoopbackFile(fd int) fs.FileHandle {
+	return &loopbackFile{fd: fd}
+}
+
+func (n *LoopbackNode) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+	flags = flags &^ syscall.O_APPEND
+	p := n.path()
+	f, err := syscall.Open(p, int(flags), 0)
+	if err != nil {
+		return nil, 0, fs.ToErrno(err)
+	}
+	lf := NewLoopbackFile(f)
+	return lf, 0, 0
+}
+
+var _ = (fs.NodeOpendirer)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Opendir(ctx context.Context) syscall.Errno {
+	fd, err := syscall.Open(n.path(), syscall.O_DIRECTORY, 0755)
+	if err != nil {
+		return fs.ToErrno(err)
+	}
+	syscall.Close(fd)
+	return fs.OK
+}
+
+var _ = (fs.NodeReaddirer)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
+	return fs.NewLoopbackDirStream(n.path())
+}
+
+var _ = (fs.NodeGetattrer)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Getattr(ctx context.Context, f fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	if f != nil {
+		return f.(fs.FileGetattrer).Getattr(ctx, out)
+	}
+
+	p := n.path()
+
+	var err error
+	st := syscall.Stat_t{}
+	if &n.Inode == n.Root() {
+		err = syscall.Stat(p, &st)
+	} else {
+		err = syscall.Lstat(p, &st)
+	}
+
+	if err != nil {
+		return fs.ToErrno(err)
+	}
+	out.FromStat(&st)
+	return fs.OK
+}
+
+var _ = (fs.NodeSetattrer)((*LoopbackNode)(nil))
+
+func (n *LoopbackNode) Setattr(ctx context.Context, f fs.FileHandle, in *fuse.SetAttrIn, out *fuse.AttrOut) syscall.Errno {
+	p := n.path()
+	fsa, ok := f.(fs.FileSetattrer)
+	if ok && fsa != nil {
+		fsa.Setattr(ctx, in, out)
+	} else {
+		if m, ok := in.GetMode(); ok {
+			if err := syscall.Chmod(p, m); err != nil {
+				return fs.ToErrno(err)
+			}
+		}
+
+		uid, uok := in.GetUID()
+		gid, gok := in.GetGID()
+		if uok || gok {
+			suid := -1
+			sgid := -1
+			if uok {
+				suid = int(uid)
+			}
+			if gok {
+				sgid = int(gid)
+			}
+			if err := syscall.Chown(p, suid, sgid); err != nil {
+				return fs.ToErrno(err)
+			}
+		}
+
+		mtime, mok := in.GetMTime()
+		atime, aok := in.GetATime()
+
+		if mok || aok {
+
+			ap := &atime
+			mp := &mtime
+			if !aok {
+				ap = nil
+			}
+			if !mok {
+				mp = nil
+			}
+			var ts [2]syscall.Timespec
+			ts[0] = fuse.UtimeToTimespec(ap)
+			ts[1] = fuse.UtimeToTimespec(mp)
+
+			if err := syscall.UtimesNano(p, ts[:]); err != nil {
+				return fs.ToErrno(err)
+			}
+		}
+
+		if sz, ok := in.GetSize(); ok {
+			if err := syscall.Truncate(p, int64(sz)); err != nil {
+				return fs.ToErrno(err)
+			}
+		}
+	}
+
+	fga, ok := f.(fs.FileGetattrer)
+	if ok && fga != nil {
+		fga.Getattr(ctx, out)
+	} else {
+		st := syscall.Stat_t{}
+		err := syscall.Lstat(p, &st)
+		if err != nil {
+			return fs.ToErrno(err)
+		}
+		out.FromStat(&st)
+	}
+	return fs.OK
 }
 
 /////
