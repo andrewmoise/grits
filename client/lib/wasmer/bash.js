78bfc8c20e30ca742453334b3f1db54e12d7fe52
Andrew Moise <moise@melanic.org>
2026-06-27 18:37:39 +0000

    Some WIP on Wasmer things which don't work

--- client/lib/wasmer/bash.js
+++ client/lib/wasmer/bash.js
@@ -1,9 +1,12 @@
-const { Wasmer, init, initializeLogger } = await import("@wasmer/sdk");
+const { Wasmer, init, initializeLogger, Directory } = await import("@wasmer/sdk");
 import { Terminal } from "@xterm/xterm";
 import { FitAddon } from "@xterm/addon-fit";
 
+const encoder = new TextEncoder();
+
 async function main() {
-    await init();
+    const wasmUrl = new URL('/lib/node_modules/@wasmer/sdk/dist/wasmer_js_bg.wasm', import.meta.url).href;
+    await init({ module: wasmUrl });
     initializeLogger("debug");
 
     const term = new Terminal({ cursorBlink: true, convertEol: true });
@@ -15,15 +18,24 @@ async function main() {
     term.writeln("Loading sharrattj/bash...");
 
     const pkg = await Wasmer.fromRegistry("sharrattj/bash");
+    term.reset();
     term.writeln("Starting bash...");
 
-    const instance = await pkg.entrypoint.run();
+    const home = new Directory();
+
+    const instance = await pkg.entrypoint.run({
+        uses: ["wasmer/neatvi"],
+        mount: { "/home": home },
+        cwd: "/home",
+    });
 
     term.writeln("Connected.");
+    connectStreams(instance, term);
+}
 
-    const encoder = new TextEncoder();
-    const stdin = instance.stdin.getWriter();
-    term.onData(data => stdin.write(encoder.encode(data)));
+function connectStreams(instance, term) {
+    const stdin = instance.stdin?.getWriter();
+    term.onData(data => stdin?.write(encoder.encode(data)));
     instance.stdout.pipeTo(new WritableStream({ write: chunk => term.write(chunk) }));
     instance.stderr.pipeTo(new WritableStream({ write: chunk => term.write(chunk) }));
 }
