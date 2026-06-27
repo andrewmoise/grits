aaa3926b23cfad4ed7e0a233fc8fdfa0d3f86324
Andrew Moise <moise@melanic.org>
2026-06-27 20:03:21 +0000

    Make launch() able to take a GimbalPath for a specific widget impl to launch

--- client/lib/launch/main.js
+++ client/lib/launch/main.js
@@ -6,15 +6,21 @@ export async function invoke(gimbal, prev, widgetName, ...args) {
   let path = null;
   if (prev instanceof GimbalPath) {
     path = prev;
-  } else if (typeof args[0] === 'string' && (args[0].startsWith('/') || args[0].startsWith('//'))) {
-    path = gimbal.p(args[0]);
   }
 
-  const mod = await import(`../${widgetName}/gwm-widget.js`);
-  await window.gimbal.openWidget(mod, {
-    name: widgetName,
-    gimbal,
-    path,
-    zone: 'master',
-  });
+  let mod, name;
+  if (widgetName instanceof GimbalPath) {
+    const r = gimbal.resolvePath(widgetName.abs());
+    const url = `${gimbal._serverUrl}/grits/v1/content/${r.volumeName}/${r.path}`;
+    mod = await import(url);
+    name = widgetName.toString();
+    if (!path && args[0] instanceof GimbalPath) path = args[0];
+  } else {
+    mod = await import(`../${widgetName}/gwm-widget.js`);
+    name = widgetName;
+    if (!path && typeof args[0] === 'string' && (args[0].startsWith('/') || args[0].startsWith('//')))
+      path = gimbal.p(args[0]);
+  }
+
+  await window.gimbal.openWidget(mod, { name, gimbal, path, zone: 'master' });
 }
